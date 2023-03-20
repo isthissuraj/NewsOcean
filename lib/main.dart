741ebf5403dc189698889fbb4d65627700a97d34
isthissuraj <surajnegi1704@gmail.com>
2023-03-20 23:44:17 +0530

    webView implemented

--- lib/main.dart
+++ lib/main.dart
@@ -16,7 +16,7 @@ class MyApp extends StatelessWidget {
       debugShowCheckedModeBanner: false,
       title: 'News Ocean',
       theme: ThemeData(
-        primarySwatch: Colors.blue,
+        primarySwatch: Colors.deepPurple,
       ),
       home: const HomeScreen(),
     );
